29f0b6c66cb85e90ab29f83fa1d72187e1f60dc7
wallacesintra <wallvxwallace962@gmail.com>
2025-11-18 12:51:22 +0300

    feat: Add bottom space to school selection screen
    This commit adds a `Spacer` to the bottom of the `LazyColumn` in the `SelectSchool` composable. This prevents the floating action button from overlapping the last item in the list of schools, ensuring all options are fully visible and accessible to the user.

--- app/src/main/java/com/nyansapoai/teaching/presentation/onboarding/components/SelectSchool.kt
+++ app/src/main/java/com/nyansapoai/teaching/presentation/onboarding/components/SelectSchool.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
@@ -85,6 +87,10 @@ fun SelectSchool(
                         )
                     }
                 }
+            }
+
+            item {
+                Spacer(Modifier.height(120.dp))
             }
         }
 
